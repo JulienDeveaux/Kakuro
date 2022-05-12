f3fecc29aeff3ea4b4ec6ea1f182d2db88e8cf19
julienDeveaux <julien.deveaux@hotmail.com>
2022-05-12 18:21:02 +0200

    Permutation marche

--- main.py
+++ main.py
@@ -58,8 +58,10 @@ def main(tableauResolution=None, tableauIndice=None):
         for j in range(tailleGrille):
             if tableauResolution[i][j] == 0:
                 tableauResolution[i][j] = random.randint(1, 9)
-    print(F([tableauResolution, tableauIndice]))
     printResultat(tableauResolution)
+    print(getConstraints(2, 2, [tableauResolution, tableauIndice]))
+    voisiny = voisin([tableauResolution, tableauIndice])
+    printResultat(voisiny[0])
     start_time = time.time()
     res = recuit([tableauResolution, tableauIndice])
     stop_time = time.time() - start_time
@@ -71,7 +73,7 @@ def main(tableauResolution=None, tableauIndice=None):
 def recuit(X0):
     X = X0
     T = 1000  # plus c'est haut plus longtemps on peut remonter la courbe (diminue au fur et à mesure)
-    Nt = 100  # nb d'iteration
+    Nt = 100  # nb d'itération
     while F(X) != 0:
         for i in range(0, Nt):
             Y = voisin(X)
@@ -89,14 +91,90 @@ def decroissance(T):
     return value
 
 
-def voisin(x):
-    pos = random.randint(0, tailleGrille - 1)
-    pos2 = random.randint(0, tailleGrille - 1)
-    while x[0][pos][pos2] == "vide":
-        pos = random.randint(0, tailleGrille - 1)
-        pos2 = random.randint(0, tailleGrille - 1)
-    x[0][pos][pos2] = random.randint(1, 9)
+def voisin(x):  # fais en sorte qu'à une position random chacune des contraintes associées soient respectées
+    i = random.randint(0, tailleGrille - 1)
+    j = random.randint(0, tailleGrille - 1)
+    while x[0][i][j] == "vide":
+        i = random.randint(0, tailleGrille - 1)
+        j = random.randint(0, tailleGrille - 1)
+    decompositions = getDecompositions(x, i, j)
+    for h in range(len(decompositions[0][0])):          # on remet la décomposition dans les bonnes cases en hauteur
+        x[0][decompositions[0][1]][decompositions[0][2] + h] = decompositions[0][0][h]
+    for h in range(len(decompositions[1][0])):          # on remet la décomposition dans les bonnes cases en largeur
+        x[0][decompositions[1][1] + h][decompositions[1][2]] = decompositions[1][0][h]
     return [x[0], x[1]]
+
+
+def getConstraints(i, j, tableau):
+    it = i
+    pos = tableau[0][it][j]
+    while pos != "vide":        # récupère la contrainte en haut
+        it -= 1
+        pos = tableau[0][it][j]
+    contrainteHaut = tableau[1][it][j][0]
+    it += 1
+    pos = tableau[0][it][j]
+    posStartHaut = [it, j]
+    lengthHaut = 0
+    while pos != "vide":        # récupère la longueur du nombre demandé en haut
+        lengthHaut += 1
+        it += 1
+        pos = tableau[0][it][j]
+
+    it = i
+    pos = tableau[0][i][it]
+    while pos != "vide":        # récupère la contrainte à gauche
+        it -= 1
+        pos = tableau[0][i][it]
+    contrainteGauche = tableau[1][i][it][1]
+    it += 1
+    pos = tableau[0][i][it]
+    posStartGauche = [i, it]
+    lengthGauche = 0
+    while pos != "vide":        # récupère la longueur du nombre demandé en haut
+        lengthGauche += 1
+        it += 1
+        pos = tableau[0][i][it]
+
+    offsetGauche = -(posStartGauche[1] - j)     # les offsets sont utilisés pour accorder les décompositions
+    offsetHaut = -(posStartHaut[0] - i)         # sur une valeur à un endroit précis
+    return [[contrainteGauche, lengthGauche, offsetGauche, posStartGauche],
+            [contrainteHaut, lengthHaut, offsetHaut, posStartHaut]]
+
+
+def getDecompositions(tableau, i, j):
+    contraintes = getConstraints(i, j, tableau)
+    decomposition1 = decomposition(contraintes[0][0], contraintes[0][1])
+    decomposition2 = decomposition(contraintes[1][0], contraintes[1][1])
+    positionDansCtr1 = contraintes[0][2]
+    positionDansCtr2 = contraintes[1][2]
+    while(decomposition1[positionDansCtr1] != decomposition2[positionDansCtr2]):    # fais en sorte qu'a la position random choisie dans voisin les deux décompositions aient la même valeur
+        decomposition1 = decomposition(contraintes[0][0], contraintes[0][1])
+        decomposition2 = decomposition(contraintes[1][0], contraintes[1][1])
+    startingPoint1 = contraintes[0][3][0]
+    startingPoint2 = contraintes[1][3][0]
+    endingPoint1 = contraintes[0][3][1]
+    endingPoint2 = contraintes[1][3][1]
+    return [[decomposition1, startingPoint1, endingPoint1], [decomposition2, startingPoint2, endingPoint2]]
+
+
+def decomposition(num, subNum):
+    res = [None] * subNum
+    cumulSum = 0
+    subSection = 0
+    max_random_number = int(num / subNum)
+    while True:
+        random_number = random.randint(1, max_random_number)
+        res[subSection] = random_number
+        cumulSum += random_number
+        num -= random_number
+        subSection += 1
+        if subSection == subNum - 1:
+            random_number = num
+            res[subSection] = random_number
+            cumulSum += random_number
+            break
+    return res
 
 
 def F(x):  # calcul combien de contraintes son non satisfaites
